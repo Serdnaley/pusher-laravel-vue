b0ebf24a8ec20f6c71f1b4ac351e13f788ba3610
Andres <serdnaley@gmail.com>
2020-05-15 11:35:30 +0300

    setup base client layout, styling and configurations

---
+++ client/src/components/layouts/BaseLayout.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-row class="base-layout">
+        <el-col :md="6" class="base-layout__sidebar">
+            <slot name="sidebar"/>
+        </el-col>
+        <el-col :md="18" class="base-layout__content">
+            <slot name="content"/>
+        </el-col>
+    </el-row>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Direct extends Vue {
+        @Prop() private msg!: string;
+    }
+</script>
+
+<style lang="scss">
+    .base-layout {
+        height: 100vh;
+        overflow: hidden;
+
+        &__sidebar {
+            padding: 15px;
+            height: 100%;
+            background: $--color-black;
+            overflow-y: auto;
+        }
+
+        &__content {
+            padding: 15px;
+            overflow-y: auto;
+        }
+    }
+</style>
--- client/vue.config.js
+++ client/vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
     productionSourceMap: false,
     css: {
         sourceMap: true,
+        loaderOptions: {
+            scss: {
+                prependData: `@import "~@/assets/scss/_var.scss";`,
+            },
+        }
     }
 };
