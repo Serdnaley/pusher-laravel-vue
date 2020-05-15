5cfbaf0376e6a716d375683decdcc0fd5965f898
Andres <serdnaley@gmail.com>
2020-05-15 10:13:28 +0300

    setup base client layout, styling and configurations

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+
+import VueRouterBackButton from 'vue-router-back-button'
+import {loadProgressBar} from 'axios-progress-bar'
+
+import 'axios-progress-bar/dist/nprogress.css'
+import './assets/scss/style.scss'
+
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import CustomLoader from './plugins/custom-loader'
+
+Vue.use(ElementUI);
+Vue.use(VueAxios, axios);
+Vue.use(VueRouterBackButton, {router});
+Vue.use(CustomLoader);
+
+loadProgressBar();
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,27 +1,23 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import VueRouter, {RouteConfig} from 'vue-router'
 
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        name: 'Home',
+        redirect: '/direct',
+    },
+    {
+        path: '/direct',
+        name: 'Direct',
+        component: () => import('@/views/direct/index.vue'),
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
